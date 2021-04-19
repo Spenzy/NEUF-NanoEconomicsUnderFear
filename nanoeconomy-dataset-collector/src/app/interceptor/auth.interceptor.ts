59b48286b6ae81bffd9de778d218a8a839879201
Spenzy <zieddr@gmail.com>
2021-04-19 16:55:25 +0100

    user profile (testing component)

--- nanoeconomy-dataset-collector/src/app/interceptor/auth.interceptor.ts
+++ nanoeconomy-dataset-collector/src/app/interceptor/auth.interceptor.ts
@@ -9,11 +9,11 @@ const TOKEN_HEADER = 'Authorization';       // for Spring Boot back-end
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private token: AuthTokenService) { }
+  constructor(private authToken: AuthTokenService) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
+    const token = this.authToken.getToken();
     if (token != null) {
       authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER, 'Bearer ' + token) });
     }

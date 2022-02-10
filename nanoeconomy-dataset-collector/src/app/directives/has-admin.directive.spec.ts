259347b12f51d1a5f13001563c7f08b6af4b3df7
Dridi Zied <37536894+Spenzy@users.noreply.github.com>
2022-02-10 21:43:31 +0100

    Delete nanoeconomy-dataset-collector directory

--- nanoeconomy-dataset-collector/src/app/directives/has-admin.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import {HasAdminDirective} from './has-admin.directive';
-
-describe('HasAdminDirective', () => {
-  it('should create an instance', () => {
-    // @ts-ignore
-    const directive = new HasAdminDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- nanoeconomy-dataset-collector/src/app/pipes/language.pipe.spec.ts
+++
@@ -1,9 +0,0 @@
-import {LanguagePipe} from './language.pipe';
-
-describe('LanguagePipe', () => {
-  it('create an instance', () => {
-    // @ts-ignore
-    const pipe = new LanguagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});

f5cef69429271a17b5cdb9ba7b3f15c5f139b87f
Dridi Zied <37536894+Spenzy@users.noreply.github.com>
2022-02-10 21:44:36 +0100

    Delete src directory

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  jwtSecret: "noideawhattoputasasecrettoken",
-  refreshSecret: "henlodisisdarefreshtokensankyou",
-  db: "mongodb+srv://Admin:TIdJJeAf1eMYnhfD@dass21dc-db.pgyfh.mongodb.net/innoDB?retryWrites=true&w=majority",
-  host: "http://localhost",
-  port: 8080
-};
